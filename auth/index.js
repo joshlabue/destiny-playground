46b5dec804f4d6ec22e96b224b5b48545d58e3ce
Josh LaBue <joshua@labue.me>
2020-10-02 21:55:00 -0400

    auth waits to respond until username is resolved

--- auth/index.js
+++ auth/index.js
@@ -6,13 +6,15 @@ let fs = require('fs');
 
 let fetch = require('node-fetch');
 
-const client_id = 31383; // change this to your own client ID
+const config = require('./config.json');
+
+const client_id = config.oAuthID; // change this to your own client ID
 
 app.get('/bnetAuth', async(req, res) => {
     if(req.query.code) {
-        res.send('good<br><button onclick="document.location=\'/bnetAuth\'">again</button>')
-        console.log('recieved code. can get oauth token now.');
-
+        let resBody = '';
+        // console.log('recieved code. can get oauth token now.');
+        
         try {
             const apiResponse = await fetch('https://www.bungie.net/Platform/App/OAuth/Token/', {
                 method: 'post',
@@ -20,25 +22,33 @@ app.get('/bnetAuth', async(req, res) => {
                 headers: {'Content-Type': 'application/x-www-form-urlencoded'}
             });
 
-            console.log('fetch is done ');
+            // console.log('fetch is done ');
 
             const apiResponseBody = await apiResponse.json();
 
-            console.log(apiResponseBody);
+            const userQuery = await fetch('https://www.bungie.net/Platform/User/GetCurrentBungieNetUser/', {
+                method: 'get',
+                headers: {
+                    'X-API-Key': config.apiKey,
+                    'Authorization': `Bearer ${apiResponseBody.access_token}`
+                }
+            });
 
+            const userQueryResponse = await userQuery.json();
+
+            resBody = `authenticated as ${userQueryResponse['Response']['displayName']}`
         }
         catch(e) {
             console.log('fetch error');
+            resBody = `error authenticating. please try again.`
             console.log(e);
         }
-        
 
+        res.send(resBody + '\n<br><button onclick="document.location=\'/bnetAuth\'">again</button>')
     }
     else {
         res.redirect(`https://www.bungie.net/en/OAuth/Authorize?client_id=${client_id}&response_type=code`);
     }
-    // res.send('done');
-    // res.send(req.params['code']);
 });
 
 https.createServer({
